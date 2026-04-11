cffc1fc2c9be3d8a8070c788967ae2152bfe42e8
Nika Oragvelidze <oragvelidzeniko.2024@gmail.com>
2026-04-11 19:19:17 +0400

    Add README

--- TaskManagerAPI/obj/Debug/net10.0/TaskManagerAPI.AssemblyInfo.cs
+++ TaskManagerAPI/obj/Debug/net10.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4487f63b9faf9b989b9c3b6ff70f93e03b026c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
